cc905c95bc119289f6170ea810fa33f3966d8723
Sam <sam.germain@usask.ca>
2020-02-23 14:22:14 -0800

    Fixed merge bug

--- components/Pages/SearchPage.js
+++ components/Pages/SearchPage.js
@@ -1,41 +1,28 @@
-<<<<<<< HEAD
 import React from 'react';
 import { StyleSheet, Text, TextInput, View, ImageBackground } from 'react-native';
 import SliderBox from '../SliderBox'
 import SearchButton from '../SearchButton'
 import RNPickerSelect from 'react-native-picker-select'
 import {airportList, getAirportCityNameAndCodeMap} from '../AirportList.js'
-import DatePicker from '../MyDatePicker'
-=======
-import React from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
-import SliderBox from "../SliderBox";
-import SearchButton from "../SearchButton";
-import RNPickerSelect from "react-native-picker-select";
-import { airportList, getAirportCityNameAndCodeMap } from "../AirportList.js";
->>>>>>> d927bcfa7e5936fcc1dc4d2059f43c752b31d97d
 
-const SearchText = props => {
-  return (
-    <View style={styles.searchText}>
-      <Text>{props.text}</Text>
-    </View>
-  );
-};
+
+const SearchText = (props) => {
+  return <View style={styles.searchText}><Text>{props.text}</Text></View>
+}
 
 class SearchBox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.navigation = props.navigation;
+
+  constructor(props){
+    super(props)
+    this.navigation = props.navigation
   }
 
-  render() {
-    state: {
-      origin: "";
-      destination: "";
+    render(){
+      state:{
+        origin:"";
+        destination:"";
     }
     return (
-<<<<<<< HEAD
       <ImageBackground style={styles.background} source={require('../../assets/airplane.jpg')} style={{width: '100%', height: '100%'}}>
         <View style={styles.searchBox}>
             <View style={styles.searchRow}>
@@ -64,62 +51,30 @@ class SearchBox extends React.Component {
                 {/* <DatePicker /> */}
                 <TextInput style={styles.searchField} onChangeText={text => this.setState({departureDate:text})} ></TextInput>
                 {/* <Text style={styles.searchText}>→</Text>
-=======
-      <View style={styles.searchBox}>
-        <View style={styles.searchRow}>
-          <SearchText text="From: " />
-          {/* <TextInput style={styles.searchField} onChangeText={text => this.setState({origin:text})} ></TextInput> */}
-          <RNPickerSelect
-            style={styles.container}
-            onValueChange={label => this.setState({ origin: label })}
-            items={getAirportCityNameAndCodeMap(airportList)}
-          />
-        </View>
-        <View style={styles.searchRow}>
-          <SearchText text="To: " />
-          <RNPickerSelect
-            style={styles.container}
-            onValueChange={label => this.setState({ destination: label })}
-            items={getAirportCityNameAndCodeMap(airportList)}
-          />
-        </View>
-        <View style={styles.searchRow}>
-          <SearchText text="Date : " />
-          {/* <DatePicker /> */}
-          <TextInput
-            style={styles.searchField}
-            onChangeText={text => this.setState({ departureDate: text })}
-          ></TextInput>
-          {/* <Text style={styles.searchText}>→</Text>
->>>>>>> d927bcfa7e5936fcc1dc4d2059f43c752b31d97d
                 <TextInput style={styles.searchField} onChangeText={text => this.setState({endDate:text})} ></TextInput> */}
+            </View>
+            <SliderBox />
+            <SearchButton navigation={this.navigation} getArguments={this.state} />
         </View>
-<<<<<<< HEAD
         </ImageBackground>
         );
     }
-=======
-        <SliderBox />
-        <SearchButton navigation={this.navigation} getArguments={this.state} />
-      </View>
-    );
-  }
->>>>>>> d927bcfa7e5936fcc1dc4d2059f43c752b31d97d
 }
 
 /**
  * Page to search for flights and display the WC Grade for each flight
  */
 class SearchPage extends React.Component {
-  state = { search: "" };
+  
+    state = {search:""}
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <SearchBox navigation={this.props.navigation} />
-      </View>
-    );
-  }
+    render(){
+        return (
+            <View style={styles.container}>
+                <SearchBox navigation={this.props.navigation} />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
@@ -131,20 +86,14 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-<<<<<<< HEAD
     
     backgroundColor: '#a3eeff',
     alignItems: 'center',
     justifyContent: 'center',
-=======
-    backgroundColor: "#aaa",
-    alignItems: "center",
-    justifyContent: "center"
->>>>>>> d927bcfa7e5936fcc1dc4d2059f43c752b31d97d
   },
   searchButton: {
     marginTop: 20,
-    backgroundColor: "#24D9E8",
+    backgroundColor: '#24D9E8',
     borderRadius: 5,
     padding: 5
   },
@@ -153,17 +102,13 @@ const styles = StyleSheet.create({
     borderRadius: 1
   },
   searchBox: {
-<<<<<<< HEAD
     alignItems: "center",
     marginTop: 60
-=======
-    alignItems: "center"
->>>>>>> d927bcfa7e5936fcc1dc4d2059f43c752b31d97d
   },
   searchField: {
-    width: 100,
-    height: 25,
-    backgroundColor: "#fff"
+      width: 100,
+      height: 25,
+      backgroundColor: '#fff'
   },
   searchRow: {
     flexDirection: "row",
@@ -172,9 +117,9 @@ const styles = StyleSheet.create({
   },
   searchText: {
     width: 50,
-    justifyContent: "center",
-    alignItems: "flex-end"
+    justifyContent: 'center',
+    alignItems: 'flex-end'
   }
 });
 
-export default SearchPage;
+export default SearchPage
